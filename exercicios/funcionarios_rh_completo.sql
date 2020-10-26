5916c45afa0ef3220be9d326e19481e24934e403
Herick Willians <herickwill@hotmail.com>
2020-10-26 15:05:04 -0300

    exercicios mysql

--- exercicios/funcionarios_rh_completo.sql
+++ exercicios/funcionarios_rh_completo.sql
@@ -14,18 +14,16 @@ create table funcionarios (
  insert into funcionarios (nome,idade,area,salario) values 
 	('Evandro',20,'Adm',2500.50),
 	('Gabriele',14,'TI',16500.40),
-	('Josicleiton',42,'Produção',1045.70),
+	('Claudio',42,'Produção',1045.70),
 	('Tatiane',30,'Engenharia',6300.50),
-	('Naruto',19,'TI',850.40);
+	('Pedro',19,'TI',850.40);
  
 #salarios maiores do que 2000
 select * from funcionarios where salario > 2000;  
 #salarios menores do que 2000
 select * from funcionarios where salario < 2000;  
-#atualizando a coluna 'salario' da tabela 'funcionarios' no id 5 (no caso, naruto)
+#atualizando a coluna 'salario' da tabela 'funcionarios' no id 5 (no caso, Pedro)
 	update funcionarios 
 		set salario = 4500.33 
 		where id = 5;
-    
-    
     
